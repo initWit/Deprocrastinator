79644d177c350b1082b393fe5f91168c27cc903a
initWit <figueras.robert@gmail.com>
2014-05-20 08:29:12 -0500

    MVP Stretches Bug Fix Complete

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -14,12 +14,13 @@
 @property NSMutableArray *toDoArray;
 @property (weak, nonatomic) IBOutlet UITableView *toDoTableView;
 
-@property int indexPathRowToBeDeleted;
+@property NSInteger indexPathRowToBeDeleted;
+
+@property NSMutableArray *toDoItemPriorityArray;
 
 @end
 
 @implementation ViewController
-
 
 
 - (void)viewDidLoad
@@ -27,34 +28,26 @@
     [super viewDidLoad];
 	self.toDoArray = [NSMutableArray arrayWithObjects:@"Ignore Don",
                       @"Buy Kevin Lunch",
-                      @"Maker the early train",
+                      @"Make the early train",
                       @"Don't sleep in tomorrow", nil];
+
+    self.toDoItemPriorityArray = [NSMutableArray arrayWithObjects:
+                                  [NSNumber numberWithInt:0],
+                                  [NSNumber numberWithInt:0],
+                                  [NSNumber numberWithInt:0],
+                                  [NSNumber numberWithInt:0], nil];
 
     [self.navigationController setNavigationBarHidden:YES];
 }
 
 
-- (IBAction)onEditButtonPressed:(id)sender
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    UIButton *editButton = sender;
-    if([editButton.currentTitle isEqualToString:@"Edit" ])
-    {
-    [editButton setTitle:@"Done" forState:UIControlStateNormal];
-        [self.toDoTableView setEditing:YES animated:YES];
-    }
-    else if([editButton.currentTitle isEqualToString:@"Done" ])
-    {
-        [editButton setTitle:@"Edit" forState:UIControlStateNormal];
-        [self.toDoTableView setEditing:NO animated:NO];
-    }
-
-}
-
-- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
 
     if (buttonIndex == 1) {
 
         [self.toDoArray removeObjectAtIndex:self.indexPathRowToBeDeleted];
+        [self.toDoItemPriorityArray removeObjectAtIndex:self.indexPathRowToBeDeleted];
         [self.toDoTableView reloadData];
 
     }
@@ -62,7 +55,39 @@
 }
 
 
-#pragma mark - Table Delegate Methods
+#pragma mark - TableView Delegate Methods
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.toDoArray.count;
+}
+
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ToDoCell"];
+    cell.textLabel.text = [self.toDoArray objectAtIndex:indexPath.row];
+
+    if ([self.toDoItemPriorityArray objectAtIndex:indexPath.row] == [NSNumber numberWithInt:1]) {
+        cell.textLabel.textColor = [UIColor whiteColor];
+        cell.backgroundColor = [UIColor greenColor];
+    }
+    else if ([self.toDoItemPriorityArray objectAtIndex:indexPath.row] == [NSNumber numberWithInt:2]) {
+        cell.textLabel.textColor = [UIColor whiteColor];
+        cell.backgroundColor = [UIColor yellowColor];
+    }
+    else if ([self.toDoItemPriorityArray objectAtIndex:indexPath.row] == [NSNumber numberWithInt:3]) {
+        cell.textLabel.textColor = [UIColor whiteColor];
+        cell.backgroundColor = [UIColor redColor];
+    }
+    else {
+        cell.textLabel.textColor = [UIColor blackColor];
+        cell.backgroundColor = [UIColor whiteColor];
+    }
+
+    return cell;
+}
+
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
 
@@ -74,10 +99,8 @@
                                                           cancelButtonTitle:@"Cancel"
                                                           otherButtonTitles:@"Delete", nil];
     [confirmDeleteAlertView show];
-
-
-
 }
+
 
 -(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
 {
@@ -87,27 +110,19 @@
             [self.toDoArray removeObjectAtIndex:sourceIndexPath.row];
             [self.toDoArray insertObject:stringToMove atIndex:destinationIndexPath.row];
 
+            NSNumber *priorityToMove = [self.toDoItemPriorityArray objectAtIndex:sourceIndexPath.row];
+            [self.toDoItemPriorityArray removeObjectAtIndex:sourceIndexPath.row];
+            [self.toDoItemPriorityArray insertObject:priorityToMove atIndex:destinationIndexPath.row];
         }
 }
 
--(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return self.toDoArray.count;
-}
-
--(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ToDoCell"];
-    cell.textLabel.text = [self.toDoArray objectAtIndex:indexPath.row];
-
-    return cell;
-}
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
 
     if([self.editButton.currentTitle isEqualToString:@"Done"])
     {
         [self.toDoArray removeObjectAtIndex:indexPath.row];
+        [self.toDoItemPriorityArray removeObjectAtIndex:indexPath.row];
         [tableView reloadData];
     }
 }
@@ -115,16 +130,42 @@
 
 #pragma mark - Helper Methods
 
+- (IBAction)onCancelButtonPressed:(id)sender {
+    [self.inputTextField resignFirstResponder];
+}
+
 - (IBAction)onAddButtonPressed:(id)sender
 {
-    NSString *nextToDoItem = self.inputTextField.text;
-    [self.toDoArray addObject:nextToDoItem];
-    [self.inputTextField resignFirstResponder];
-    self.inputTextField.text = @"";
+    if (self.inputTextField.text.length != 0) {
 
-    [self.toDoTableView reloadData];
+        NSString *nextToDoItem = self.inputTextField.text;
+        [self.toDoArray addObject:nextToDoItem];
+        [self.toDoItemPriorityArray addObject:[NSNumber numberWithInt:0]];
+
+        [self.inputTextField resignFirstResponder];
+        self.inputTextField.text = @"";
+
+        [self.toDoTableView reloadData];
+    }
+}
+
+- (IBAction)onEditButtonPressed:(id)sender
+{
+    UIButton *editButton = sender;
+
+    if([editButton.currentTitle isEqualToString:@"Edit List" ])
+    {
+        [editButton setTitle:@"Done" forState:UIControlStateNormal];
+        [self.toDoTableView setEditing:YES animated:YES];
+    }
+    else if([editButton.currentTitle isEqualToString:@"Done" ])
+    {
+        [editButton setTitle:@"Edit List" forState:UIControlStateNormal];
+        [self.toDoTableView setEditing:NO animated:NO];
+    }
     
 }
+
 
 - (IBAction)onSwipeGesture:(UISwipeGestureRecognizer *)swipeGestureRecognizer
 {
@@ -135,32 +176,38 @@
             CGPoint swipeLocation = [swipeGestureRecognizer locationInView:self.toDoTableView];
             NSIndexPath *swipeIndex = [self.toDoTableView indexPathForRowAtPoint:swipeLocation];
             [self.toDoArray removeObjectAtIndex:swipeIndex.row];
+            [self.toDoItemPriorityArray removeObjectAtIndex:swipeIndex.row];
 
             [self.toDoTableView reloadData];
         }
     }
 
     if (swipeGestureRecognizer.direction == UISwipeGestureRecognizerDirectionRight) {
-        NSLog(@"YOU SWIPED RIGHT");
 
         if (swipeGestureRecognizer.state == UIGestureRecognizerStateEnded) {
             CGPoint swipeLocation = [swipeGestureRecognizer locationInView:self.toDoTableView];
             NSIndexPath *swipeIndex = [self.toDoTableView indexPathForRowAtPoint:swipeLocation];
-
             UITableViewCell *swipedCell = [self.toDoTableView cellForRowAtIndexPath:swipeIndex];
+
             if (swipedCell.textLabel.textColor == [UIColor blackColor]) {
-                swipedCell.textLabel.textColor = [UIColor whiteColor];
                 swipedCell.backgroundColor = [UIColor greenColor];
+                swipedCell.textLabel.textColor = [UIColor whiteColor];
+                [self.toDoItemPriorityArray replaceObjectAtIndex:swipeIndex.row withObject:[NSNumber numberWithInt:1]];
             }
             else if (swipedCell.backgroundColor == [UIColor greenColor]) {
                 swipedCell.backgroundColor = [UIColor yellowColor];
+                swipedCell.textLabel.textColor = [UIColor whiteColor];
+                [self.toDoItemPriorityArray replaceObjectAtIndex:swipeIndex.row withObject:[NSNumber numberWithInt:2]];
             }
             else if (swipedCell.backgroundColor == [UIColor yellowColor]) {
                 swipedCell.backgroundColor = [UIColor redColor];
+                swipedCell.textLabel.textColor = [UIColor whiteColor];
+                [self.toDoItemPriorityArray replaceObjectAtIndex:swipeIndex.row withObject:[NSNumber numberWithInt:3]];
             }
             else if (swipedCell.backgroundColor == [UIColor redColor]) {
                 swipedCell.textLabel.textColor = [UIColor blackColor];
                 swipedCell.backgroundColor = [UIColor whiteColor];
+                [self.toDoItemPriorityArray replaceObjectAtIndex:swipeIndex.row withObject:[NSNumber numberWithInt:0]];
             }
 
             [self.toDoTableView reloadData];
